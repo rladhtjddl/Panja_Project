17b9246aa544e0a775312324bdeabd4133ca3e2c
rladhtjd <rladh@DESKTOP-E1MJQNF>
2018-11-27 14:30:51 +0900

    json ftp put및 파일 클라이드 익스플로러 기능 ver0.003추가 ㅎㅎㅎ

--- Panja_Project/Program.cs
+++ Panja_Project/Program.cs
@@ -42,7 +42,7 @@ namespace Panja_Project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new SignUp());
+            Application.Run(new MainUI());
 
             
 
