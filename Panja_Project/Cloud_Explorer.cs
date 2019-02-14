10bcfa14ebd31c1f0f8195bd11207651cba72e47
동근 장 <rootofeast4@gmail.com>
2019-02-15 00:05:22 +0900

    업로드 버튼도 만듬

--- Panja_Project/Cloud_Explorer.cs
+++ Panja_Project/Cloud_Explorer.cs
@@ -262,5 +262,46 @@ namespace Panja_Project
         {
           
         }
+
+        private void btn_upload_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog upload = new FolderBrowserDialog();
+            upload.ShowDialog();
+            string select_folder = upload.SelectedPath;
+            UploadDirectory(sftp, select_folder, ".");
+            MessageBox.Show("Upload Complete");
+
+        }
+
+        void UploadDirectory(SftpClient client, string localPath, string remotePath)
+        {
+            Console.WriteLine("Uploading directory {0} to {1}", localPath, remotePath);
+
+            IEnumerable<FileSystemInfo> infos =
+                new DirectoryInfo(localPath).EnumerateFileSystemInfos();
+            foreach (FileSystemInfo info in infos)
+            {
+                if (info.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    string subPath = remotePath + "/" + info.Name;
+                    if (!client.Exists(subPath))
+                    {
+                        client.CreateDirectory(subPath);
+                    }
+                    UploadDirectory(client, info.FullName, remotePath + "/" + info.Name);
+                }
+                else
+                {
+                    using (Stream fileStream = new FileStream(info.FullName, FileMode.Open))
+                    {
+                        Console.WriteLine(
+                            "Uploading {0} ({1:N0} bytes)",
+                            info.FullName, ((FileInfo)info).Length);
+
+                        client.UploadFile(fileStream, remotePath + "/" + info.Name);
+                    }
+                }
+            }
+        }
     }
 }
